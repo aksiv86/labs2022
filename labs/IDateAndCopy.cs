9685b1a6cb0d1193b906a30aacc39b790ea4fb69
aksiv86 <Ivan.imom@yandex.ru>
2022-12-18 22:17:44 +0300

    Initial commit

---
+++ labs/IDateAndCopy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs
+{
+    //определяем интерфейс IDateAndCopy. Добавим его реализацию в другие классы
+    interface IDateAndCopy
+    {
+        object DeepCopy();
+        DateTime Date {get;set;} 
+    }
+}
